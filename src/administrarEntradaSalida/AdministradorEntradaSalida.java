95a862e7ac954721621867445d88a833a71a93db
Ismael Prieto <ismael.prieto@urv.cat>
2021-11-05 12:51:23 +0100

    Comentarios añadidos y control de errores en ficheros actualizados

--- src/administrarEntradaSalida/AdministradorEntradaSalida.java
+++ src/administrarEntradaSalida/AdministradorEntradaSalida.java
@@ -6,14 +6,29 @@ import java.io.*;
 import java.util.Objects;
 import java.util.Scanner;
 
+
 public class AdministradorEntradaSalida {
-    public String[] leer(String nomFit) {
+
+    /**
+     * Constructor vacío
+     */
+    public AdministradorEntradaSalida() {
+
+    }
+
+    /**
+     * Lee información de un fichero con formato CSV
+     *
+     * @param nomFichero nombre del fichero del que se leera la información
+     * @return devuelve una lista de Strings con los números leídos del fichero
+     */
+    public String[] leer(String nomFichero) {
         int nums = 0, j = 0;
         try {
-            BufferedReader f = new BufferedReader(new FileReader(nomFit));
+            BufferedReader f = new BufferedReader(new FileReader(nomFichero));
             int numLineas = (int) f.lines().count();
             f.close();
-            Scanner ft = new Scanner(new File(nomFit));
+            Scanner ft = new Scanner(new File(nomFichero));
             String[] aux = new String[numLineas];
             int i;
             for (i = 0; i < numLineas; i++) {
@@ -25,24 +40,31 @@ public class AdministradorEntradaSalida {
                 }
             }
             ft.close();
-
             String[] numeros = new String[nums];
             for (i = 0; i < nums; i++) numeros[i] = aux[i];
             return numeros;
-
         } catch (IOException e) {
             return null;
         }
-
     }
 
-    public void escribir(GestionPrimos gestor, String nomFit) throws IOException {
-
-        BufferedWriter fresul = new BufferedWriter(new FileWriter(nomFit));
-
-        fresul.write("Numero;Primo;Tiempo(ms)\n");
-        fresul.write(gestor.toStringFichero());
-        fresul.close();
-
+    /**
+     * Escribe la información almacenada en un objeto de clase GestionPrimos en un fichero nuevo que tendrá
+     * el nombre introducido por parámetro.
+     *
+     * @param gestor     será el parámetro del que obtendremos la información a escribir
+     * @param nomFichero será el nombre que tomará el fichero en el que escribamos
+     * @return booleano indicando si se ha escrito correctamente o ha habido un error con el fichero
+     */
+    public boolean escribir(GestionPrimos gestor, String nomFichero) {
+        try {
+            BufferedWriter fresul = new BufferedWriter(new FileWriter(nomFichero));
+            fresul.write("Numero;Primo;Tiempo(ms)\n");
+            fresul.write(gestor.toStringFichero());
+            fresul.close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
 }
--- src/mains/Test.java
+++ src/mains/Test.java
@@ -3,21 +3,21 @@ package mains;
 import administrarEntradaSalida.AdministradorEntradaSalida;
 import procesado.GestionPrimos;
 
-import java.io.*;
-
 
 public class Test {
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
         AdministradorEntradaSalida administrador = new AdministradorEntradaSalida();
         System.out.println("Ejecutando primer test...");
         String[] test1 = administrador.leer("fichero.txt");
         if (test1 != null) {
             GestionPrimos gestor = new GestionPrimos(test1);
             gestor.procesarPrimos();
-            administrador.escribir(gestor, "resultados.csv");
+            if (!administrador.escribir(gestor, "resultados.csv")) {
+                System.out.println("Error escribiendo en fichero");
+            }
+        } else {
+            System.out.println("Error leyendo del fichero");
         }
-
     }
 }
